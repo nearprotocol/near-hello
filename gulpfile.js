2b21f909805194fb3f8a5743c0e69d105bf21515
Willem Wyndham <willem@cs.umd.edu>
2019-11-16 17:45:57 -0500

    Changed main file to index.ts

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,7 @@ const gulp = require("gulp");
 const nearUtils = require("near-bindgen-as/compiler");
 
 function build_wasm(done){
-  nearUtils.compile("./assembly/main.ts", "./dist/main.wasm", done);
+  nearUtils.compile("./assembly/index.ts", "./dist/main.wasm", done);
 };
 
 const build = gulp.series(build_wasm);
